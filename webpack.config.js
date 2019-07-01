66c31a08a5d38e5030b35a2c191b66b05350bf78
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-07-01 00:33:34 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, './')
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['main.*.js']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    watch: true,
+    devServer: {
+        overlay: true,
+        writeToDisk: true
+    }
+};
