3d95303db16f084bc6e867adacaa1cd5282df0cf
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-07-04 15:34:25 -0700

    update mock units

--- src/worlds/world1.js
+++ src/worlds/world1.js
@@ -8,33 +8,34 @@ export default {
         {
             type: 'catapult',
             owner: 0,
-            x: 0,
-            y: 0
-        },
-        {
-            type: 'catapult',
-            owner: 0,
-            x: 0,
-            y: 8
+            x: 4,
+            y: 2
         },
         {
             type: 'townhall',
             owner: 0,
-            x: 2,
-            y: 3
+            x: 3,
+            y: 6
         }
+        /*
+        {
+            type: 'farm',
+            owner: 0,
+            x: 10,
+            y: 10
+        },
+        {
+            type: 'catapult',
+            owner: 0,
+            x: 12,
+            y: 8
+        },
         /*
         {
             type: 'peasant',
             owner: 0,
             x: 6,
             y: 10
-        },
-        {
-            type: 'farm',
-            owner: 0,
-            x: 4,
-            y: 6
         },
         {
             type: 'peasant',
